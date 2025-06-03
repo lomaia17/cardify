4626bb44947abf110a65818f89d43f49de916f2c
Giorgi Lomaia <giorgilomaia1710@gmail.com>
2025-06-03 09:49:19 +0400

    update firebase config

--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -4,19 +4,25 @@ import { getFirestore, collection, addDoc  , getDocs, query, where , deleteDoc ,
 
 // Firebase configuration from Firebase Console
 const firebaseConfig = {
-    apiKey: "AIzaSyAhLdC5DkxeJ3eg4Yvm7drV1ySNCgOQoa8",
-    authDomain: "business-card-761a2.firebaseapp.com",
-    projectId: "business-card-761a2",
-    storageBucket: "business-card-761a2.firebasestorage.app",
-    messagingSenderId: "879115763102",
-    appId: "1:879115763102:web:253aee58a322cf55332a8f"
+  apiKey: "AIzaSyDmyd7hsxqmp99GdOruHVKmdp7LKB5bPco",
+  authDomain: "r2-businesscard.firebaseapp.com",
+  projectId: "r2-businesscard",
+  storageBucket: "r2-businesscard.firebasestorage.app",
+  messagingSenderId: "158464506372",
+  appId: "1:158464506372:web:67499dee6f77498da0d351"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
-setPersistence(auth, browserLocalPersistence);
-const provider = new GoogleAuthProvider();
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    // Now authentication methods can be called
+    // For example, trigger sign-in method if required
+  })
+  .catch((error) => {
+    console.error("Error setting persistence:", error);
+  });
 
-export { db , collection, addDoc , auth, provider ,getDocs, query, where , deleteDoc , doc , getDoc, getAuth, onAuthStateChanged};
+export { db , collection, addDoc , auth ,getDocs, query, where , deleteDoc , doc , getDoc, getAuth, onAuthStateChanged};
 export type { User };
